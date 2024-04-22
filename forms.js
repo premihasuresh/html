9ca43cedb12cb502c0db9b5a57cb5ff732e46c3d
premiha <premihasuresh28@gmail.com>
2024-04-22 17:37:01 +0530

    first commit

---
+++ forms.js
@@ -0,0 +1,26 @@
+function display(){
+
+    let fname=document.getElementById("fname").value
+    let firstname=document.getElementById("firstname")
+    firstname.innerHTML=fname
+
+    let lname=document.getElementById("lname").value
+    let lastname=document.getElementById("lastname")
+    lastname.innerHTML=lname
+
+    let email=document.getElementById("email").value
+    let emailid=document.getElementById("E-mail")
+    emailid.innerHTML=email
+
+    let age=document.getElementById("age").value
+    let Age=document.getElementById("Age")
+    Age.innerHTML=age
+
+    let phoneno=document.getElementById("phoneno").value
+    let mob=document.getElementById("mob")
+    mob.innerHTML=phoneno
+
+    let address=document.getElementById("address").value
+    let Address=document.getElementById("Address")
+    Address.innerHTML=address
+    }
